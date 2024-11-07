e7fd388daa7b65d77ff0e6ad685e5555edbf627a
hisham-kitaz <111257875+hisham-kitaz@users.noreply.github.com>
2024-11-07 01:34:55 -0500

    introduced new fonts. Added button functionality to main menu buttons

---
+++ Chromatic Journey/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    // Loads Level 1
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene("Level1"); // Make sure the scene names match exactly
+    }
+
+    // Loads Level 2
+    public void LoadLevel2()
+    {
+        SceneManager.LoadScene("Level2");
+    }
+
+    // Loads Level 3
+    public void LoadLevel3()
+    {
+        SceneManager.LoadScene("Level3");
+    }
+}
