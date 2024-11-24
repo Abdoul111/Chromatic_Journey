7faacf8ad7c89c7dc572cd5b7590abd57a328593
Alta Sheikh <98616697+altam-sh@users.noreply.github.com>
2024-11-24 09:10:13 -0500

    Signs Logic Implemented in Level 2

---
+++ Chromatic Journey/Assets/Scripts/SignManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SignManager : MonoBehaviour
+{
+    [SerializeField] public string popUpString;
+    [SerializeField] private AudioClip audioPopup1;
+    private AudioSource audioSource;
+    private bool hasInteracted = false;
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player") && !hasInteracted)
+        {
+            hasInteracted = true;
+
+            if (audioPopup1 != null && !audioSource.isPlaying)
+            {
+                audioSource.PlayOneShot(audioPopup1);
+            }
+
+            //PopupText pop = GameObject.FindGameObjectWithTag("PopupManager").GetComponent<PopupText>();
+            //pop.PopUp(popUpString);
+            PopupText.Instance.PopUp(popUpString);
+        }
+    }
+}
